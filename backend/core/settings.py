adee851b23345adb240391f304d8c14b952fd827
sdhm <shanccom@unsa.edu.pe>
2025-05-06 17:14:02 -0500

    App Clientes

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #OTROS
+    'rest_framework',
+
+    #APPS CREADAS
+    'ventas',
+    'clientes',
 ]
 
 MIDDLEWARE = [
