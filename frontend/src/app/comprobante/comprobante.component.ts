26abb78531b34514ac3fd616a1f65604bb7d1923
sdhm <shanccom@unsa.edu.pe>
2025-05-26 17:16:24 -0500

    Eliminacion del backend

--- frontend/src/app/comprobante/comprobante.component.ts
+++ frontend/src/app/comprobante/comprobante.component.ts
@@ -4,14 +4,18 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-comprobante',
   standalone: true,
-  imports: [FormsModule, RouterModule, HttpClientModule],
+  imports: [FormsModule, RouterModule, HttpClientModule, CommonModule],
   templateUrl: './comprobante.component.html',
   styleUrl: './comprobante.component.css' 
 })
+
+
 export class ComprobanteComponent {
 
   tipoComprobante: string = '01';  
@@ -20,6 +24,24 @@ export class ComprobanteComponent {
   estadoSunat: string = 'pendiente'; 
   fechaEmision: Date = new Date(); 
 
+  cliente = {
+    tipo_doc: '',
+    num_doc: '',
+    rzn_social: ''
+  };
+
+  items = [
+    {
+      tipo_producto: 'accesorio',
+      producto_accesorio: {
+        codigo: '',
+        descripcion: ''
+      },
+      precio_unitario: 0,
+      cantidad: 1
+    }
+  ];
+
   constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
     // Accedemos a los parámetros de la ruta
     this.activatedRoute.params.subscribe(params => {
@@ -27,32 +49,31 @@ export class ComprobanteComponent {
     });
   }
 
+  eliminarItem(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  agregarItem() {
+    this.items.push({
+      tipo_producto: 'accesorio',
+      producto_accesorio: {
+        codigo: '',
+        descripcion: ''
+      },
+      precio_unitario: 0,
+      cantidad: 1
+    });
+  }
+
   crearComprobante() {
     const comprobanteData = {
-      venta: {
-        cliente: {
-          tipo_doc: "6",  
-          num_doc: "12345678",  
-          rzn_social: "Nombre del Cliente" 
-        }
-      },
-      tipo_comprobante: this.tipoComprobante,  
+      cliente: this.cliente,
+      tipo_comprobante: this.tipoComprobante,
       serie: this.serie,
       correlativo: this.correlativo,
       estado_sunat: this.estadoSunat,
-      fecha_emision: this.fechaEmision.toISOString(), 
-      detalles: [
-        {
-          tipo_producto: "accesorio",  
-          producto_accesorio: {
-            codigo: "ABC123",  
-            descripcion: "Descripción del accesorio"
-          },
-          precio_unitario: 100.0,  
-          cantidad: 1  
-        }
-        
-      ]
+      fecha_emision: this.fechaEmision.toISOString(),
+      items: this.items
     };
     
     console.log('Datos que se enviarán al backend:', JSON.stringify(comprobanteData, null, 2));
