c6998e1fc4fd6fc2cf9d263672f887f534a7b3d6
DeniseHuacani <dhuacanij@unsa.edu.pe>
2025-05-25 19:11:36 -0500

    dashboard: Se implemento cards de estadísticas (ventas y ganancia), falta integrar backend

--- frontend/src/app/dashboard/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StatsCardComponent } from '../stats-card/stats-card.component'
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, StatsCardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  ventasSemana = 120;
+  ventasMes = 460;
+  gananciaDia = 340.50;
 
+  //luego se le mandaria los valores del backend
 }
---
+++ frontend/src/app/dashboard/stats-card/stats-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-stats-card',
+  imports: [CommonModule],
+  templateUrl: './stats-card.component.html',
+  styleUrl: './stats-card.component.css'
+})
+export class StatsCardComponent {
+  @Input() icon = '';
+  @Input() title = '';
+  @Input() value: string | number | null = '';
+}
