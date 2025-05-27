f5765b3a0b166356536645e196a33b864800f6bd
DeniseHuacani <dhuacanij@unsa.edu.pe>
2025-05-27 09:16:39 -0500

    Se creo el componente Inventario, agregar a las rutas, tabla de productos y filtros

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { ComprobanteComponent } from './comprobante/comprobante.component';
+import { InventarioComponent } from './inventario/inventario.component'
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'inventario', component: InventarioComponent },
   { path: 'factura', component: ComprobanteComponent },
   { path: '**', redirectTo: 'login' }
 ];
