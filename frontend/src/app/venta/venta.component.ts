25649f52038bc776a6cd036fbfc1704386a1f89b
sdhm <shanccom@unsa.edu.pe>
2025-05-28 00:33:07 -0500

    Implementacion de la app scanner - Falta configurar algunos detalles, ademas que no funciona muy bien con la camara de laptop se tiene que usar una externa, si es webcam es dificil enfocar en cambio si es de celular es mas facil

--- frontend/src/app/venta/venta.component.ts
+++ frontend/src/app/venta/venta.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ScannerComponent } from '../shared/scanner/scanner.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-venta',
-  imports: [RouterModule],
+  imports: [RouterModule, ScannerComponent, CommonModule],
   templateUrl: './venta.component.html',
   styleUrl: './venta.component.css'
 })
 export class VentaComponent {
-
+  
+  handleScan(scannedCode: string): void {
+    console.log('Código escaneado recibido:', scannedCode);
+    // Aquí puedes procesar el código escaneado
+    // Por ejemplo: buscar el producto, agregarlo al carrito, etc.
+  }
 }
