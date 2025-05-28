631e7243eb37b08a945d945f0e1822a132ef229b
DeniseHuacani <dhuacanij@unsa.edu.pe>
2025-05-28 18:18:33 -0500

    Configurar API para inventario con productos (accesorios, lunas, monturas)
    Se implementaron serializadores y vista 'ProductosView'. Añadido CORS en configuración y endpoint '/productos/' para acceso desde Angular

--- backend/inventario/views.py
+++ backend/inventario/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Montura, Luna, Accesorio
+from .serializers import MonturaSerializer, LunaSerializer, AccesorioSerializer
 # Create your views here.
+
+class ProductosView(APIView):
+    def get(self, request):
+        monturas = MonturaSerializer(Montura.objects.all(), many=True).data
+        lunas = LunaSerializer(Luna.objects.all(), many=True).data
+        accesorios = AccesorioSerializer(Accesorio.objects.all(), many=True).data
+        return Response(monturas + lunas + accesorios)
