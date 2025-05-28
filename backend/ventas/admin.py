ca8f775df0519257bacfd5096d85a813b6bbf476
FabbPP <ffpacheco239@gmail.com>
2025-05-27 22:43:10 -0500

    Ya modelos completados

--- backend/ventas/admin.py
+++ backend/ventas/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from ventas.models import Empleado, Venta
+from ventas.models import Empleado, Venta, DetalleVentaLuna, DetalleVentaMontura, DetalleVentaAccesorio, BoletaElectronica
 # Register your models here.
 admin.site.register(Empleado)
 admin.site.register(Venta)
+admin.site.register(DetalleVentaLuna)
+admin.site.register(DetalleVentaMontura)
+admin.site.register(DetalleVentaAccesorio)
+admin.site.register(BoletaElectronica)
