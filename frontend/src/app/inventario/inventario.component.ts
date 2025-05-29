f5ff5c1307869a891f402d4834d967e75b76bf1f
DeniseHuacani <dhuacanij@unsa.edu.pe>
2025-05-28 21:21:49 -0500

    Añadir estilos  a la interfaz de inventario tabla

--- frontend/src/app/inventario/inventario.component.ts
+++ frontend/src/app/inventario/inventario.component.ts
@@ -10,30 +10,29 @@ import { InventarioService } from '../services/inventario.service';
   styleUrl: './inventario.component.css'
 })
 export class InventarioComponent implements OnInit {
-  // corregir
-  productos: any[] =[];
-  /*
-  productos = [
-    { codigo: 'P001', marca: 'Marca A', material: 'Madera', color: 'Rojo', precio: 100, estado: 'Disponible' },
-    { codigo: 'P002', marca: 'Marca B', material: 'Plástico', color: 'Verde', precio: 150, estado: 'Vendido' },
-    { codigo: 'P003', marca: 'Marca C', material: 'Metal', color: 'Azul', precio: 200, estado: 'Disponible' },
-    
-  ];*/
+  
+  productos: any[] = [];
+  productosFiltrados: any[] = [];
+  
   constructor(private inventarioService: InventarioService) {}
 
   ngOnInit(): void {
-    
-   this.inventarioService.obtenerProductos().subscribe(data => {
-      console.log('Datos recibidos desde el backend:', data);  // Aquí vemos los datos que estamos recibiendo
-      this.productos = data;
-    }, (error) => {
-      console.error('Error al obtener productos:', error);
-    });
+    this.inventarioService.obtenerProductos().subscribe(
+      (data) => {
+        console.log('Datos recibidos desde el backend:', data);
+        this.productos = data;
+        this.productosFiltrados = [...data];
+      },
+      (error) => {
+        console.error('Error al obtener productos:', error);
+      }
+    );
   }
 
-  productosFiltrados = [...this.productos];
 
+  // Filtros
   filtros = {
+    tipo: '',
     marca: '',
     material: '',
     color: '',
@@ -42,22 +41,25 @@ export class InventarioComponent implements OnInit {
   };
 
 // Métodos
+   // Aplicar filtros
   aplicarFiltros(filtrosAplicados: any) {
     this.filtros = { ...this.filtros, ...filtrosAplicados };
+
     this.productosFiltrados = this.productos.filter(producto => {
       return (
-        (this.filtros.marca ? producto.marca.includes(this.filtros.marca) : true) &&
-        (this.filtros.material ? producto.material.includes(this.filtros.material) : true) &&
-        (this.filtros.color ? producto.color.includes(this.filtros.color) : true) &&
-        (this.filtros.precio.min !== null ? producto.precio >= this.filtros.precio.min : true) &&
-        (this.filtros.precio.max !== null ? producto.precio <= this.filtros.precio.max : true) &&
-        (this.filtros.estado ? producto.estado === this.filtros.estado : true)
+        (this.filtros.tipo ? producto.tipo.includes(this.filtros.tipo) : true) &&  // Filtra por tipo
+        (this.filtros.marca ? producto.marca.includes(this.filtros.marca) : true) &&  // Filtra por marca
+        (this.filtros.material ? producto.material.includes(this.filtros.material) : true) &&  // Filtra por material
+        (this.filtros.color ? producto.color.includes(this.filtros.color) : true) &&  // Filtra por color
+        (this.filtros.precio.min !== null ? producto.proCosto >= this.filtros.precio.min : true) &&  // Filtra por precio mínimo
+        (this.filtros.precio.max !== null ? producto.proPrecioVenta <= this.filtros.precio.max : true) &&  // Filtra por precio máximo
+        (this.filtros.estado ? producto.estado === this.filtros.estado : true)  // Filtra por estado
       );
     });
   }
-
   resetFiltros() {
     this.filtros = {
+      tipo: '',
       marca: '',
       material: '',
       color: '',
