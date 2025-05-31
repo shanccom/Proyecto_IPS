e375db68a6fee289bb670f0befae45522fb7e7a3
FabbPP <ffpacheco239@gmail.com>
2025-05-31 15:11:11 -0500

    Modelo Usuario

--- backend/ventas/models.py
+++ backend/ventas/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from cliente.models import Cliente
 from django.utils import timezone
 from inventario.models import Montura, Luna, Accesorio
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from rest_framework.authtoken.models import Token
 
 class Empleado(models.Model):
@@ -11,18 +11,45 @@ class Empleado(models.Model):
         ('Inactivo', 'inactivo'),
     ]
     CARGO= [
-        ('Gerente', 'Gerente'),
-        ('Colaborador', 'Colaborador'),
+        ('Gerente', 'gerente'),
+        ('Colaborador', 'colaborador'),
     ]
     emplCod = models.AutoField(primary_key = True)
     emplNom = models.CharField(max_length = 50)
     emplCarg = models.CharField(max_length = 20, choices=CARGO)
-    empCond = models.CharField(choices = CONDICION_EMPLEADO)
+    empCond = models.CharField(choices = CONDICION_EMPLEADO, max_length=20)
     
     def __str__(self):
-        return str( self.emplCod) + " " + str( self.emplNom) + " " + str( self.emplCargo)
+        return f"{self.emplCod} {self.emplNom} {self.emplCarg}"
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, usuarioNom, password, emplCod):
+        if not usuarioNom:
+            raise ValueError("El usuario debe tener un nombre")
+        
+        user = self.model(usuarioNom=usuarioNom, emplCod=emplCod)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, usuarioNom, password, emplCod):
+        user = self.create_user(usuarioNom, password, emplCod)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
+class Usuario(AbstractBaseUser):
+    usuarioNom = models.CharField(unique= True, max_length = 10)
+    emplCod = models.OneToOneField(Empleado, related_name='Dueño', on_delete=models.CASCADE)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    objects = CustomUserManager()
+    is_active = models.BooleanField(default=True)
+    USERNAME_FIELD = "usuarioNom"
+    REQUIRED_FIELDS = ["emplCod"]
+
+    def __str__(self):
+        return str( self.usuarioNom)
     
 class Venta(models.Model):
     TIPO_PAGO = [
@@ -36,7 +63,7 @@ class Venta(models.Model):
         ('Cancelado', 'cancelado'),
     ]
     ventNum = models.AutoField(primary_key = True)
-    cliCod = models.OneToOneField(Cliente, on_delete=models.CASCADE, related_name='compras')    #Cuando se elimina cliente se eliminan sus recetas
+    cliCod = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='compras')    #Cuando se elimina cliente se eliminan sus recetas
     empCod = models.ForeignKey(Empleado, on_delete=models.CASCADE, related_name='ventas')
     venFecha = models.DateTimeField(default=timezone.now)
     venTipoPago = models.CharField(choices=TIPO_PAGO)
