6404b8458ce799a930ac5571429b1808dcaa6c05
FabbPP <ffpacheco239@gmail.com>
2025-06-01 14:13:38 -0500

    Usuario views urls permisos, y eliminacion de Lunas en inventario

--- backend/OP_SYSTEM/urls.py
+++ backend/OP_SYSTEM/urls.py
@@ -19,10 +19,9 @@ from django.urls import path
 from django.urls import path, include
 from inventario.views import ProductosView #url 
 from usuario.views import login  #Para poner login como pagina principal para pruebas tokens
-from inventario.views import obtener_opciones_filtros
 from inventario.views import ProductosView #url products
 from inventario.views import obtener_filtros_accesorio, obtener_filtros_montura
-from inventario.views import crear_montura, crear_luna, crear_accesorio 
+from inventario.views import crear_montura, crear_accesorio 
 
 
 urlpatterns = [
@@ -35,6 +34,5 @@ urlpatterns = [
     path('productoslista/filtros/accesorio/', obtener_filtros_accesorio),
     #path('productoslista/filtros/',obtener_opciones_filtros),
     path('monturas/', crear_montura),
-    path('lunas/', crear_luna),
     path('accesorios/', crear_accesorio),
 ]
