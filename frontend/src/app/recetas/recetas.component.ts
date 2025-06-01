1b41e4a8cb8374e68deecc453da90d448798fc09
DeniseHuacani <dhuacanij@unsa.edu.pe>
2025-06-01 17:47:15 -0500

    Creacion de componente recetas y ajuste en sidebar

---
+++ frontend/src/app/recetas/recetas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recetas',
+  imports: [],
+  templateUrl: './recetas.component.html',
+  styleUrl: './recetas.component.css'
+})
+export class RecetasComponent {
+
+}
