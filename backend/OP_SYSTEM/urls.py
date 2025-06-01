ab9d38cb6285c3a66af39e33dc3ca13c63c339d1
FabbPP <ffpacheco239@gmail.com>
2025-06-01 15:22:38 -0500

    Modelos correctos en ventas, correcion errores

--- backend/OP_SYSTEM/urls.py
+++ backend/OP_SYSTEM/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('productos/', include('inventario.urls')),
     path('usuario/', include('usuario.urls')),
+        #Denise :v pon tus url en el product url para que este ordenado
     path('productoslista/', ProductosView.as_view(), name='productos'),
     path('productoslista/filtros/montura/', obtener_filtros_montura),
     path('productoslista/filtros/accesorio/', obtener_filtros_accesorio),
