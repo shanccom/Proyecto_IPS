30262e6d63f631b7cabc8f92865118303667913b
FabbPP <ffpacheco239@gmail.com>
2025-06-01 11:59:17 -0500

    Usuario app

---
+++ backend/usuario/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Empleado, Venta, BoletaElectronica, Usuario
+#Usuario 
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['NombreUser', 'empleado', 'apellidos']
+        
+    def create(self, validated_data):
+        return Usuario.objects.create_user(**validated_data)
+    
+class EmpleadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Empleado
+        fields = [
+            'emplCod', 'emplNom', 'emplCarg', 'empCond'  
+        ]
