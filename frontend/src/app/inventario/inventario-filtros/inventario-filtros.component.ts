56b46e812f4985774d87cb3a2a99f7a333fa331d
DeniseHuacani <dhuacanij@unsa.edu.pe>
2025-06-01 13:38:25 -0500

    Correcion de inventario para mostrar las tablas y filtros segun el producto

--- frontend/src/app/inventario/inventario-filtros/inventario-filtros.component.ts
+++ frontend/src/app/inventario/inventario-filtros/inventario-filtros.component.ts
@@ -1,10 +1,10 @@
-import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit,Input, SimpleChanges,OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { InventarioService } from '../../services/inventario.service';
 import { HttpClientModule } from '@angular/common/http';
 
-type CampoFiltro = 'tipo' | 'marca' | 'material' | 'color' | 'estado';
+type CampoFiltro = 'marca' | 'material' | 'color' | 'estado' | 'publico';
 
 @Component({
   selector: 'app-inventario-filtros',
@@ -12,56 +12,81 @@ type CampoFiltro = 'tipo' | 'marca' | 'material' | 'color' | 'estado';
   templateUrl: './inventario-filtros.component.html',
   styleUrl: './inventario-filtros.component.css'
 })
-export class InventarioFiltrosComponent implements OnInit {
+export class InventarioFiltrosComponent implements OnInit, OnChanges {
   @Output() filtrosAplicados = new EventEmitter<any>();
   @Output() reset = new EventEmitter<void>();
+  @Input() tipo: 'montura' | 'accesorio' = 'montura';
 
-  campos: CampoFiltro[] = ['tipo', 'marca', 'material', 'color', 'estado'];
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['tipo']) {
+      this.cargarFiltros();
+    }
+  }
+
+  campos: CampoFiltro[] = [ 'marca', 'material','publico', 'color', 'estado'];
 
   seleccionados: Record<CampoFiltro, string[]> & { precio: { min: number; max: number } } = {
-    tipo: [],
     marca: [],
     material: [],
+    publico:[],
     color: [],
     estado: [],
     precio: { min: 0, max: 0 }
   };
 
   desplegar: Record<CampoFiltro, boolean> = {
-    tipo: false,
     marca: false,
     material: false,
+    publico:false,
     color: false,
     estado: false
   };
 
   opcionesFiltros: Record<CampoFiltro, string[]> = {
-    tipo: [],
     marca: [],
     material: [],
+    publico:[],
     color: [],
     estado: []
   };
 
   constructor(private inventarioService: InventarioService) {}
+  marcasMontura: string[] = [];
+  publicos: string[] = [];
+  materiales: string[] = [];
+  colores: string[] = [];
+
 
   ngOnInit(): void {
-    this.inventarioService.obtenerOpcionesFiltros().subscribe(
-      (data) => {
-        console.log('Datos recibidos desde el backend:', data);
-        this.opcionesFiltros = {
-          tipo: data.tipos || [],
-          marca: data.marcas || [],
-          material: data.materiales || [],
-          color: data.colores || [],
-          estado: data.estados || []
-        };
-      },
-      (error) => {
-        console.error('Error al obtener filtros:', error);
-      }
-    );
+    this.cargarFiltros();
   }
+
+  private cargarFiltros(): void {
+    this.inventarioService.obtenerOpcionesFiltros(this.tipo).subscribe((res) => {
+      if (this.tipo === 'montura') {
+        this.campos = ['marca', 'material', 'publico', 'color', 'estado'];
+        this.opcionesFiltros.marca = res.marcas;
+        this.opcionesFiltros.publico = res.publicos;
+        this.opcionesFiltros.material = res.materiales;
+        this.opcionesFiltros.color = res.colores;
+        this.opcionesFiltros.estado = ['Vendido', 'No vendido'];
+      } else if (this.tipo === 'accesorio') {
+        this.campos = []; // No mostramos checkboxes
+        this.opcionesFiltros = {
+          marca: [],
+          material: [],
+          publico: [],
+          color: [],
+          estado: []
+        };
+      }
+
+      this.seleccionados.precio.min = res.precio_min;
+      this.seleccionados.precio.max = res.precio_max;
+    });
+  }
+
+
 
   toggleDesplegable(campo: CampoFiltro) {
     this.desplegar[campo] = !this.desplegar[campo];
@@ -79,9 +104,9 @@ export class InventarioFiltrosComponent implements OnInit {
 
   limpiar() {
     this.seleccionados = {
-      tipo: [],
       marca: [],
       material: [],
+      publico:[],
       color: [],
       estado: [],
       precio: { min: 0, max: 0 }
