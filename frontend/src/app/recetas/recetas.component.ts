5b5f12fdee176e9539fc0b52226f24c73ab34f98
DeniseHuacani <dhuacanij@unsa.edu.pe>
2025-06-01 19:59:12 -0500

    Correcion de pequeños errores, creacionde servicio y del componente recetas

--- frontend/src/app/recetas/recetas.component.ts
+++ frontend/src/app/recetas/recetas.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecetaService } from '../services/receta.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recetas',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './recetas.component.html',
   styleUrl: './recetas.component.css'
 })
-export class RecetasComponent {
+export class RecetasComponent implements OnInit{
+  recetas: any[] =[];
+  mostrarFormulario: boolean = false;
+  
+  constructor(private recetaService:RecetaService){}
 
+  ngOnInit(): void {
+    this.recetaService.getRecetas().subscribe(
+      (data) => {
+        console.log(data); 
+        this.recetas = data;
+      },
+      (error) => {
+        console.error('Error al cargar las recetas', error)
+      }
+    );
+
+  }
+
+
+
+  nuevaReceta: any = {
+    cliCod: null,
+    rectOpt: false,
+    recDIPLejos: null,
+    recfecha: '',
+    // agrega los demás campos necesarios aquí...
+  };
+
+  abrirFormulario() {
+    this.mostrarFormulario = true;
+  }
 }
