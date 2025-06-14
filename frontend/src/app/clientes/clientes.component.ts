64bbdd1ec15606ffdb606e3b1d1d9abd064191b8
DeniseHuacani <dhuacanij@unsa.edu.pe>
2025-06-14 00:10:37 -0500

    Creacion del formulario y conexion con backend

--- frontend/src/app/clientes/clientes.component.ts
+++ frontend/src/app/clientes/clientes.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FormularioClienteComponent } from './formulario-cliente/formulario-cliente.component';
 import { ClientesService } from '../services/clientes.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-clientes',
@@ -12,18 +13,23 @@ import { ClientesService } from '../services/clientes.service';
 })
 export class ClientesComponent implements OnInit {
   mostrarFormulario: boolean = false;
+  clientes: any[] = [];
   
   constructor( private clientesService: ClientesService){};
-  ngOnInit() {
-  this.clientesService.getClientes().subscribe(
-    data => {
-      console.log("Clientes : ",data);
-    },
-    error => {
-      console.error('Error:', error);
-    }
-  );
-}
+  ngOnInit(): void {
+    this.obtenerClientes();
+  }
+  obtenerClientes(): void {
+    this.clientesService.getClientes().subscribe(
+      data => {
+        this.clientes = data; 
+        console.log('Clientes obtenidos:', this.clientes); 
+      },
+      error => {
+        console.error('Error al obtener los clientes:', error);
+      }
+    );
+  }
 
   abrirFormulario() {
     this.mostrarFormulario = true;
--- frontend/src/app/clientes/formulario-cliente/formulario-cliente.component.ts
+++ frontend/src/app/clientes/formulario-cliente/formulario-cliente.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ClientesService } from '../../services/clientes.service';
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-formulario-cliente',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulario-cliente.component.html',
   styleUrl: './formulario-cliente.component.css'
 })
 export class FormularioClienteComponent {
+  @Output() cerrar = new EventEmitter<void>();
+  @Output() guardado = new EventEmitter<void>();
+
+  form:FormGroup;
+
+  constructor(private fb: FormBuilder, private clienteService: ClientesService) {
+    this.form = this.fb.group({
+      nombre_completo: [''],
+      tipo_documento: [''],  
+      numero_documento: [''],
+      numero_celular: [''],
+      edad: ['']
+
+    });
+  }
+  // Funcion para cerrar el formulario
+  cerrarFormulario() {
+    this.cerrar.emit();
+  }
+  guardarCliente() {
+    if (this.form.invalid) {
+      return;
+    }
+    const clienteData = this.form.value;
+    console.log('Datos enviados al backend:', clienteData);
+
+    this.clienteService.agregarCliente(clienteData).subscribe(
+      () => {
+        this.guardado.emit();
+        this.cerrarFormulario();
+      },
+      (error) => {
+        console.error('Error al guardar cliente:', error);
+        if (error.error) {
+          console.error('Detalles del error:', error.error); // Ver más detalles si los hay
+        }
+      }
+    );
+  }
 
 }
