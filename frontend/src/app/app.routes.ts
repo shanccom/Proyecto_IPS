8093cd1d336aa4ff29b9ce411597636a059fb063
DeniseHuacani <dhuacanij@unsa.edu.pe>
2025-06-13 20:35:29 -0500

    Creacion del componente clientes, actualizacion del sidebar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { ComprobanteComponent } from './comprobante/comprobante.component';
 import { VentaComponent } from './venta/venta.component';
 import { InventarioComponent } from './inventario/inventario.component'
-import { RecetasComponent } from './recetas/recetas.component';
+import { ClientesComponent } from './clientes/clientes.component';
 import { ListaVentasComponent } from './lista-ventas/lista-ventas.component';
 
 export const routes: Routes = [
@@ -13,7 +13,7 @@ export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'inventario', component: InventarioComponent },
   { path: 'factura', component: ComprobanteComponent },
-  { path: 'recetas', component: RecetasComponent },
+  { path: 'clientes', component: ClientesComponent },
   { path: 'venta', component:VentaComponent },
   { path: 'listaVentas', component:ListaVentasComponent },
   { path: '**', redirectTo: 'login' }
