b48c2d697c4de3eee228d8e52bd3d8128486f119
DeniseHuacani <dhuacanij@unsa.edu.pe>
2025-07-10 20:42:31 -0500

    Correcion

--- frontend/src/app/admin/configuracion/configuracion.component.ts
+++ frontend/src/app/admin/configuracion/configuracion.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TemasService } from '../../services/temas.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-configuracion',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './configuracion.component.html',
   styleUrl: './configuracion.component.css'
 })
 export class ConfiguracionComponent {
-
 }
